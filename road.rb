f93c4b1b91b9ab6468216d0a232204aa5246e3f4
Shaul Peled <shaul@peled.cc>
2017-12-13 11:40:05 +0200

    Update road.rb
    end_junctions is really just 'junctions'

--- road.rb
+++ road.rb
@@ -1,7 +1,7 @@
 
 class Road < MapObject
 	attr_reader :junctions, :speed_limit
-	def initialize(pos_x, pos_y, end_junctions, road_speed_limit=-1)
+	def initialize(pos_x, pos_y, junctions, road_speed_limit=-1)
 		super pos_x, pos_y, :movable => false
 		@junctions = junctions
 		@speed_limit = road_speed_limit
